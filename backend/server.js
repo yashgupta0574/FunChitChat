c794147f0d92fbb63d6f7c7d87a3e35bdec0bcad
Yash <yashdgupta2002@gmail.com>
2022-05-25 10:48:27 +0530

    react based frontened added

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,20 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
 const app = express();
 app.get("/", (req, res) => res.send("API IS RUNNING!!"));
-app.listen(5000, console.log("Listening on port 5000"));
+
+app.get("/api/chats", (req, res) => {
+  res.send(chats);
+});
+
+app.get("/api/chats/:id", (req, res) => {
+  const singleChat = chats.find((c) => c._id === req.params.id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, console.log(`Listening on port ${PORT}`));
