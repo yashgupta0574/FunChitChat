a4cb324246726bde9def7aa4affa47dabf833bb3
Yash <yashdgupta2002@gmail.com>
2022-05-26 23:23:00 +0530

    updated login and signup form

--- frontened/src/components/Authentication/Login.js
+++ frontened/src/components/Authentication/Login.js
@@ -17,57 +17,57 @@ const Login = () => {
 
   const history = useHistory();
 
-  const submitHandler = async () => {
-    setLoading(true);
-    if (!email || !password) {
-      toast({
-        title: "Please Fill all the Feilds",
-        status: "warning",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-      setLoading(false);
-      return;
-    }
+  // const submitHandler = async () => {
+  //   setLoading(true);
+  //   if (!email || !password) {
+  //     toast({
+  //       title: "Please Fill all the Feilds",
+  //       status: "warning",
+  //       duration: 5000,
+  //       isClosable: true,
+  //       position: "bottom",
+  //     });
+  //     setLoading(false);
+  //     return;
+  //   }
 
-    // console.log(email, password);
-    try {
-      const config = {
-        headers: {
-          "Content-type": "application/json",
-        },
-      };
+  // console.log(email, password);
+  // try {
+  //   const config = {
+  //     headers: {
+  //       "Content-type": "application/json",
+  //     },
+  //   };
 
-      const { data } = await axios.post(
-        "/api/user/login",
-        { email, password },
-        config
-      );
+  //   const { data } = await axios.post(
+  //     "/api/user/login",
+  //     { email, password },
+  //     config
+  //   );
 
-      // console.log(JSON.stringify(data));
-      toast({
-        title: "Login Successful",
-        status: "success",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-      localStorage.setItem("userInfo", JSON.stringify(data));
-      setLoading(false);
-      history.push("/chats");
-    } catch (error) {
-      toast({
-        title: "Error Occured!",
-        description: error.response.data.message,
-        status: "error",
-        duration: 5000,
-        isClosable: true,
-        position: "bottom",
-      });
-      setLoading(false);
-    }
-  };
+  // console.log(JSON.stringify(data));
+  //     toast({
+  //       title: "Login Successful",
+  //       status: "success",
+  //       duration: 5000,
+  //       isClosable: true,
+  //       position: "bottom",
+  //     });
+  //     localStorage.setItem("userInfo", JSON.stringify(data));
+  //     setLoading(false);
+  //     history.push("/chats");
+  //   } catch (error) {
+  //     toast({
+  //       title: "Error Occured!",
+  //       description: error.response.data.message,
+  //       status: "error",
+  //       duration: 5000,
+  //       isClosable: true,
+  //       position: "bottom",
+  //     });
+  //     setLoading(false);
+  //   }
+  // };
 
   return (
     <VStack spacing="10px">
