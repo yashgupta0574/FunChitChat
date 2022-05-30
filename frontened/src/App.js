7f354beb41e80348d586f0767f04a16da65d5871
Yash <yashdgupta2002@gmail.com>
2022-05-30 17:50:41 +0530

    scrollable chats added

--- frontened/src/App.js
+++ frontened/src/App.js
@@ -1,13 +1,13 @@
 import "./App.css";
 import { Route } from "react-router-dom";
-import Homepage  from "./Pages/Homepage";
-import Chatpage  from "./Pages/Chatpage";
+import Homepage from "./Pages/Homepage";
+import Chatpage from "./Pages/Chatpage";
 
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Homepage} exact/>
-      <Route path="/chats" component={Chatpage}/>
+      <Route path="/" component={Homepage} exact />
+      <Route path="/chats" component={Chatpage} />
     </div>
   );
 }
