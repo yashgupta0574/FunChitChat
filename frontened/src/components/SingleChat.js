15a1ed30a331b947e014713b79a78cca402c5d86
Yash <yashdgupta2002@gmail.com>
2022-06-01 16:05:29 +0530

    Pushing to heroku

--- frontened/src/components/SingleChat.js
+++ frontened/src/components/SingleChat.js
@@ -15,7 +15,9 @@ import animationData from "../animations/typing.json";
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000";
+//const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://funchitchat.herokuapp.com/";
+
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
